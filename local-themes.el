6ac8a72d196f466ddeaeb38758f3974ecf35178a
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2020-07-25 21:19:17 -0500

    change to zenburn

--- local-themes.el
+++ local-themes.el
@@ -2,8 +2,8 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
 ;; https://github.com/NicolasPetton/zerodark-theme
-(load-theme 'zerodark t)
-;; (load-theme 'zenburn t)
+;; (load-theme 'zerodark t)
+(load-theme 'zenburn t)
 ;; Optionally setup the modeline
 
 ;; (zerodark-setup-modeline-format)
